f4ea707902e7c105edaae02430c9b648391eedc1
Daniel <danielw96@hotmail.com>
2021-12-16 15:28:16 -0500

    new component to list the results

---
+++ food/src/components/ResultsList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const ResultsList = ({title, results}) => {
+    
+    return (
+      <View>
+          <Text style={styles.titleStyle}>{title}</Text>
+          <FlatList 
+            horizontal
+            data={results}
+            keyExtractor={(result) => result.id}
+            renderItem={({ item }) => {
+                return <Text>  {item.name}</Text>;
+            }}
+          />
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+      titleStyle: {
+          fontSize: 22,
+          fontWeight: 'bold'
+      }
+  });
+  
+  export default ResultsList;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -2,14 +2,25 @@ import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
+import ResultsList from '../components/ResultsList';
 
 import useResults from '../hooks/useResults';
+
+
 
 const SearchScreen = () => {
     
   const [searchInput, setSearchInput] = useState('');
 
   const [searchApi, results, errorMessage] = useResults();
+
+  console.log(results);
+
+  const filterResultsByPrice = (price) => {
+    return results.filter( result => {
+      return result.price === price;
+    });
+  };
   
   return (
     <View>
@@ -20,6 +31,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> :null}
       <Text>We have found {results.length} results! </Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Middle Ground" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Money go Brrrrrr" />
     </View>
   );
 };
